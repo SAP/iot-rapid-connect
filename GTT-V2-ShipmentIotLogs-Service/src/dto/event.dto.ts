c6e36fa989ea2db6be9a03a6a4bbf7a804d4f329
I505039 <26136787+tom-ring@users.noreply.github.com>
2022-12-08 12:04:31 +0000

    update payload validation

--- GTT-V2-ShipmentIotLogs-Service/src/dto/event.dto.ts
+++ GTT-V2-ShipmentIotLogs-Service/src/dto/event.dto.ts
@@ -1,13 +1,33 @@
-import { IsString } from "class-validator";
+import { IsArray, IsDateString, IsInt, IsNotEmpty, IsString } from "class-validator";
 
 export default class EventDto {
 
+    @IsNotEmpty()
     @IsString()
     private shipmentNo: string;
 
+    @IsNotEmpty()
+    @IsDateString()
+    private reportedAt: string;
+
+    @IsString()
+    private timezone: string;
+
+    @IsInt()
+    private priority: string;
+
+    @IsString()
+    private reportedBy: string;
+
+    @IsArray()
+    private eventDetails: string[];
+
     constructor() {
         this.shipmentNo = "";
-        console.log('Created new instance of EventDto');
+        this.reportedAt = "";
+        this.timezone = "";
+        this.priority = "";
+        this.reportedBy = "";
+        this.eventDetails = [];
     }
-
 }
